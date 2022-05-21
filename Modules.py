c4acf07b39b4f3e9ea61daa7ab3d6fee90e6c117
Vardges-Khachatryan <khachatryanvardges95@gmail.com>
2022-05-21 14:19:07 +0400

    Add Modules.py

---
+++ Modules.py
@@ -0,0 +1,13 @@
+import math as m # Կանչում ենք math մոդուլը որպես m 
+
+k = m.factorial(4) # k-ին վերագրում ենք 4-ի ֆակտորյալը
+print(k)
+
+from math import factorial as fa # Կանչում ենք math մոդուլից միայն factorial-ը որպես fa 
+
+d = fa(5) # d-ին վերագրում ենք 4-ի ֆակտորյալը
+print(d)
+
+import Modules1 as M # Կանչում ենք մեր ստեղծած մոդուլը
+
+M.hi()
